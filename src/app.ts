4c948b1237b2856081aba5fa0d9f02920768ceb5
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2025-12-10 13:09:14 +0600

    Solve Request Entity

--- src/app.ts
+++ src/app.ts
@@ -37,8 +37,8 @@ app.post(
   webhookController.handleWebhook
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 app.use("/uploads", express.static("uploads")); // Serve uploaded files
 
