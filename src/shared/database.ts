99b7b2fcbd6ece4213bf18caf95aa37cf0864f67
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2026-04-12 15:16:42 +0600

    Manually Admin Entry And Commented the Code

--- src/shared/database.ts
+++ src/shared/database.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import config from "../config";
+// import { User, UserRole, UserStatus, RegistrationStatus } from "../app/models";
+// import bcrypt from "bcrypt";
 
 async function connectMongoDB() {
   try {
@@ -11,6 +13,7 @@ async function connectMongoDB() {
       tlsAllowInvalidCertificates: true,
     });
     console.log("MongoDB connected successfully!");
+    // await initiateSuperAdmin();
   } catch (error) {
     console.error("MongoDB connection error:", error);
     console.log("Attempting to reconnect in 5 seconds...");
@@ -32,6 +35,30 @@ mongoose.connection.on("error", (err) => {
 mongoose.connection.on("disconnected", () => {
   console.log("Mongoose disconnected from MongoDB");
 });
+
+// async function initiateSuperAdmin() {
+//   const adminEmail = "info@brikky.net";
+
+//   const existingAdmin = await User.findOne({ email: adminEmail });
+//   if (existingAdmin) return;
+
+//   const hashedPassword = await bcrypt.hash(
+//     "12345678",
+//     Number(config.bcrypt_salt_rounds)
+//   );
+
+//   await User.create({
+//     email: adminEmail,
+//     password: hashedPassword,
+//     affiliationCondition: true,
+//     role: UserRole.ADMIN,
+//     status: UserStatus.ACTIVE,
+//     registrationStatus: RegistrationStatus.COMPLETED,
+//     isEmailVerified: true,
+//   });
+
+//   console.log("Super Admin created successfully");
+// }
 
 connectMongoDB();
 
